a6cad06727a2ca7749a44d0f2b0bd6cdd646b571
Harimad <leesh68481@gmail.com>
2022-10-30 15:37:59 +0900

    3. React에서 배열 사용하기 1 - 리스트 렌더링 (조회)

--- simpleDiary/src/App.js
+++ simpleDiary/src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
 
 function App() {
+  const dummyDiaryList = [
+    {
+      id: 1,
+      author: 'Harimad',
+      content: '리액트 너무 재밌다.',
+      created_date: new Date().getTime(),
+      emotion: 5,
+    },
+    {
+      id: 2,
+      author: 'SeSAC',
+      content: '힘내자 아자',
+      created_date: new Date().getTime(),
+      emotion: 4,
+    },
+    {
+      id: 3,
+      author: '누구누구',
+      content: '어떤 내용 내용',
+      created_date: new Date().getTime(),
+      emotion: 3,
+    },
+  ];
   return (
     <div className="App">
       <DiaryEditor className="DiaryEditor_container"></DiaryEditor>
+      <DiaryList diaryList={dummyDiaryList}></DiaryList>
     </div>
   );
 }
---
+++ simpleDiary/src/DiaryItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const DiaryItem = ({ id, author, content, emotion, created_date }) => {
+  return (
+    <div className="DiaryItem_container">
+      <div className="info">
+        <span className="author_info">
+          | 작성자 : {author} | 감정점수 : {emotion} |
+        </span>
+        <br></br>
+        <span className="date">{new Date(created_date).toLocaleString()}</span>
+      </div>
+      <div className="content">{content}</div>
+    </div>
+  );
+};
+
+export default DiaryItem;
